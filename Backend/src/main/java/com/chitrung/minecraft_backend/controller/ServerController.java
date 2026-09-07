333003392b6626f8f8eaaa28e19494b87213340b
EmTeenTrung <EmTeenTrung@users.noreply.github.com>
2026-09-07 16:17:39 +0700

    Noi api backend de sua phan status

--- Backend/src/main/java/com/chitrung/minecraft_backend/controller/ServerController.java
+++ Backend/src/main/java/com/chitrung/minecraft_backend/controller/ServerController.java
@@ -1,13 +1,27 @@
 package com.chitrung.minecraft_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class ServerController {
+    private String serverStatus = "offline";
     @GetMapping("/api/server/status")
     public Map<String, String> status() {
-    return Map.of("status", "offline");
+        return Map.of("status", serverStatus);
+    }
+    @PostMapping("/api/server/start")
+    public Map<String,String> start() {
+        serverStatus = "online";
+        return Map.of("status", serverStatus);
+    }
+    @PostMapping("/api/server/stop")
+    public Map<String,String> stop() {
+        serverStatus = "offline";
+        return Map.of("status", serverStatus);
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,14 +1,62 @@
 const commandInput = document.getElementById("commandInput");
 const terminalContent = document.getElementById("terminal-content");
 
-commandInput.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    const command = commandInput.value;
-    if (command.trim() === "") {
-      return;
+if (commandInput && terminalContent) {
+  commandInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const command = commandInput.value;
+      if (command.trim() === "") {
+        return;
+      }
+      terminalContent.innerHTML += "> " + command + "<br>";
+      terminalContent.scrollTop = terminalContent.scrollHeight;
+      commandInput.value = "";
     }
-    terminalContent.innerHTML += "> " + command + "<br>";
-    terminalContent.scrollTop = terminalContent.scrollHeight;
-    commandInput.value = "";
-  }
+  });
+}
+
+const statusTexts = document.querySelectorAll(".server-status-text");
+const statusElements = document.querySelectorAll(".server-status");
+
+function updateServerStatus(status) {
+  statusTexts.forEach((element) => {
+    element.textContent = status;
+  });
+  statusElements.forEach((element) => {
+    if (status === "online") {
+      element.classList.remove("offline");
+      element.classList.add("online");
+    } else {
+      element.classList.remove("online");
+      element.classList.add("offline");
+    }
+  });
+}
+
+fetch("http://localhost:8080/api/server/status")
+  .then((response) => response.json())
+  .then((data) => {
+    updateServerStatus(data.status);
+  });
+
+const startBtn = document.querySelector(".btn-start");
+
+startBtn.addEventListener("click", function () {
+  fetch("http://localhost:8080/api/server/start", {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      updateServerStatus(data.status);
+    });
+});
+
+const stopBtn = document.querySelector(".btn-stop");
+
+stopBtn.addEventListener("click", function () {
+  fetch("http://localhost:8080/api/server/stop", { method: "POST" })
+    .then((response) => response.json())
+    .then((data) => {
+      updateServerStatus(data.status);
+    });
 });
